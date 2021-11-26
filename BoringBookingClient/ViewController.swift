a554897109ce5a57b5749bc7fc8892cce9fea625
Dimitrov Blagoi <blagoi_dimitrov@icloud.com>
2021-11-26 19:54:38 +0300

    затупок на UITableView

--- BoringBookingClient/ViewController.swift
+++ BoringBookingClient/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0) ,
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0) ,
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+        ])
+        
+        tableView.register(MyCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.rowHeight = 100
+        tableView.estimatedRowHeight = 100
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyCell
+        cell.title.text = "aslkdjal"
+        return cell
+    }
+}
+
+class MyCell: UITableViewCell {
+    
+    var title: UILabel!
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        title = UILabel()
+        title.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(title)
+        
+        NSLayoutConstraint.activate([
+            title.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            title.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            title.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            title.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError ("init(coder:) has not been implemented")
+    }
+
+}
