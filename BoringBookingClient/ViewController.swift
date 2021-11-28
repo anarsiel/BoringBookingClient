b9f559978fdb6e59f9f05c2503dac5fb87538b7b
Dimitrov Blagoi <blagoi_dimitrov@icloud.com>
2021-11-29 01:16:49 +0300

    added showing restaurants

--- BoringBookingClient/ViewController.swift
+++ BoringBookingClient/ViewController.swift
@@ -6,16 +6,40 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
     
     var tableView: UITableView!
+    
+    var data: [Restaurant] = [Restaurant(), Restaurant()] {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.allowsSelection = true
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        setData { [weak self] (data) in
+            self?.data = data
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
 
         NSLayoutConstraint.activate([
@@ -27,8 +51,27 @@ class ViewController: UIViewController {
         
         tableView.register(MyCell.self, forCellReuseIdentifier: "cell")
         tableView.dataSource = self
-        tableView.rowHeight = 100
-        tableView.estimatedRowHeight = 100
+        tableView.estimatedRowHeight = 80
+    }
+    
+    func setData(completion:@escaping ([Restaurant]) -> ()) {
+        let url = URL(string: "https://boring-booking.herokuapp.com/restaurants")!
+
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let _ = data else { return }
+            let decoder = JSONDecoder()
+            
+//            print(String(data: data!, encoding: .utf8))
+            
+            if let jsonRestaurants = try? decoder.decode([Restaurant].self, from: data!) {
+                DispatchQueue.main.async {
+                    completion(jsonRestaurants)
+                }
+            }
+//            completion([String(data: data, encoding: .utf8)!])
+        }
+
+        task.resume()
     }
 }
 
@@ -38,18 +81,30 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return data.count
     }
     
-    
-    func tableView(
-        _ tableView: UITableView,
-        cellForRowAt indexPath: IndexPath
-    ) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyCell
-        cell.title.text = "aslkdjal"
+        
+        let x = "\(data[indexPath.row].name)"
+        print(x)
+        cell.title.text = data[indexPath.row].name
         return cell
     }
+}
+
+extension ViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        data.append(data[indexPath.row])
+//        print(indexPath.row)
+////        print(indexPath.row)
+////        tableView.deselectRow(at: indexPath, animated: true)
+//    }
+//
+//    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+//        return indexPath
+//    }
 }
 
 class MyCell: UITableViewCell {
@@ -74,4 +129,18 @@ class MyCell: UITableViewCell {
         fatalError ("init(coder:) has not been implemented")
     }
 
+}
+
+struct Restaurant: Codable {
+    var id: String
+    var name: String
+    
+    init() {
+        id = "A"
+        name = "B"
+    }
+}
+
+struct Restaurants: Codable {
+    var results: [Restaurant]
 }
